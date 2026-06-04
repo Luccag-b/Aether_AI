592b3b2bcbf31164d320245d35ec4f3abb8a5a7a
= <toshi0123.mizuguchi@gmail.com>
2026-06-04 12:27:30 -0300

    Adicionando o js e um evento no botao calcular carbono

---
+++ js/emissaoCarbono.js
@@ -0,0 +1,5 @@
+const calcularCarbono = document.getElementById('calcularCarbono');
+
+calcularCarbono.addEventListener('click', function() {
+    alert("O cálculo de créditos de carbono foi feito com sucesso! Em breve, você receberá um relatório detalhado por e-mail.");
+});
